f86f19b812af6ba93ccd6a038708424e2e49f9d2
Jubilee Christopher <j.christopher231@gmail.com>
2018-04-04 07:35:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,16 +13,12 @@ function logWhisper(string) {
 function sayHiToGrandma(string){
 return string.toLowercase()
 }
-var uppercase = "HELLO!"
- 
-uppercase.toUpperCase() === uppercase // true
- 
-var lowercase = 'hello!'
- 
-lowercase.toLowerCase() === lowercase // true
- 
-var mixedCase = 'Hi there!'
- 
-mixedCase.toLowerCase() === mixedCase // false
- 
-mixedCase.toUpperCase() === mixedCase // false
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can\'t hear you!";
+ } else if (string.toUpperCase() === string) {
+    return "YES INDEED!";
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
+}
